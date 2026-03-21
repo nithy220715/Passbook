d2f57277756a369a81f8843b54806358e8f2112a
nithy220715 <nithy220715@gmail.com>
2026-03-21 13:18:40 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,13 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Fintech App")),
-        body: const Center(child: Text("Hello Fintech 🚀")),
+        body: const Center(child: Text("Flutter v2 embedding fixed 🚀")),
       ),
     );
   }
