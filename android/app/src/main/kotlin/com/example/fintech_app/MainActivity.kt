9caa102d405f8b073271c6c510f57d6f409bb66f
nithy220715 <nithy220715@gmail.com>
2026-03-21 13:10:11 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/fintech_app/MainActivity.kt
@@ -0,0 +1,7 @@
+
+package com.example.fintech_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,16 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fintech App',
-      theme: ThemeData.dark(),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  double balance = 0;
-
-  void addIncome() {
-    setState(() => balance += 100);
-  }
-
-  void addExpense() {
-    setState(() => balance -= 50);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("💎 Fintech App")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Balance", style: TextStyle(color: Colors.grey)),
-            Text("₹${balance.toStringAsFixed(2)}",
-                style: const TextStyle(fontSize: 32)),
-            const SizedBox(height: 20),
-            ElevatedButton(onPressed: addIncome, child: Text("Add Income")),
-            ElevatedButton(onPressed: addExpense, child: Text("Add Expense")),
-          ],
-        ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Fintech App")),
+        body: const Center(child: Text("Hello Fintech 🚀")),
       ),
     );
   }
