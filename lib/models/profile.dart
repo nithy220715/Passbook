2b034f2a6edcb0677d331606f6af0defa62898a1
nithy220715 <nithy220715@gmail.com>
2026-03-24 18:49:49 +0530

    Create profile model class in profile.dart

---
+++ lib/models/profile.dart
@@ -0,0 +1,13 @@
+class Profile {
+  String userName;
+  String email;
+  String phone;
+  String profilePictureUrl;
+
+  Profile({
+    required this.userName,
+    required this.email,
+    required this.phone,
+    required this.profilePictureUrl,
+  });
+}
